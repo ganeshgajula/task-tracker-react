adedb35356c2c36f6e3209fd7d9c841556f55904
ganesh <ganeshgajula22@gmail.com>
2021-01-27 19:54:10 +0530

    refactor: creating class based components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React from "react";
 import Header from "./components/Header";
 
-function App() {
-  // const name = "Ganesh";
-  // const x = 5;
+// function App() {
+//   // const name = "Ganesh";
+//   // const x = 5;
 
-  return (
-    <div className="container">
-      <Header />
-      {/* <h1>Hello From React</h1> */}
-      {/* <h2>Hello {name}</h2> */}
-      {/* <h2>Hello {x === 5 ? "Yes" : "No"}</h2> */}
-    </div>
-  );
+//   return (
+//     <div className="container">
+//       <Header />
+//       {/* <h1>Hello From React</h1> */}
+//       {/* <h2>Hello {name}</h2> */}
+//       {/* <h2>Hello {x === 5 ? "Yes" : "No"}</h2> */}
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>;
+  }
 }
 
 export default App;
